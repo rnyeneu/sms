dcc77c37e1d46402c59f09fb656f6b722a986034
rnyeneu <nambashufwa@gmail.com>
2022-01-22 16:40:32 +0300

    Restricting un-authenticated users from accessing some pages

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,20 @@ use App\Http\Controllers\LoginController;
 Route::get('/', function () {
     return view('/auth/login');
 });
-Route::resource('student', StudentsController::class);
+
 Route::get('/auth/login', [LoginController::class, 'login'])->name('auth.login');
 Route::get('/auth/register', [LoginController::class, 'register'])->name('auth.register');
 Route::post('/auth/save', [LoginController::class, 'save'])->name('auth.save');
 Route::post('/auth/check', [LoginController::class, 'check'])->name('auth.check');
+// Route::get('/dashboard', [LoginController::class, 'dashboard'])->middleware('auth');
 Route::get('/auth/logout', [LoginController::class, 'logout'])->name('toka');
-Route::fallback('LoginController@login444');
+
+Route::get('/dashboard', function(){})->middleware('auth');
+
+Route::group(['middleware' => 'auth'], function(){
+
+Route::resource('student', StudentsController::class);
+
+});
+
+
