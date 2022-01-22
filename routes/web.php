81386a7749317635649eaee2e4dc9ba97e1630ae
rnyeneu <nambashufwa@gmail.com>
2022-01-22 16:39:10 +0300

    Restrict pages to unauthorized users

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,5 @@ Route::get('/auth/login', [LoginController::class, 'login'])->name('auth.login')
 Route::get('/auth/register', [LoginController::class, 'register'])->name('auth.register');
 Route::post('/auth/save', [LoginController::class, 'save'])->name('auth.save');
 Route::post('/auth/check', [LoginController::class, 'check'])->name('auth.check');
-Route::get('/dashboard', [LoginController::class, 'dashboard'])->middleware('auth');
 Route::get('/auth/logout', [LoginController::class, 'logout'])->name('toka');
 Route::fallback('LoginController@login444');
